095220a1b510772087579848901823691d5d0f02
Anna <anna.ryzhko@outlook.com>
2026-05-12 21:25:11 +0300

    Revert "Merge pull request #10 from wvlerv/feature/analytics-and-metrics"
    This reverts commit 51578cd416775be3be45a05ffbf978882c78510f, reversing
    changes made to a488a1c4bf56f6e36b893e828be065227e7c39af.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import MenuCatalog from './components/MenuCatalog';
 import OrdersPage from './components/OrdersPage';
 import mockDishes from './data/mockDishes.json';
-import NutritionChart from './components/NutritionChart';
 
 function App() {
   const [page, setPage] = useState('menu');
@@ -14,11 +13,6 @@ function App() {
     <div className="App">
       <Header onNavigate={setPage} currentPage={page} />
       <main className="App__main">
-        <div className="chart-container">
-          <NutritionChart />
-        </div>
-
-        <MenuCatalog dishes={mockDishes} />
         {page === 'menu' && <MenuCatalog dishes={mockDishes} />}
         {page === 'orders' && <OrdersPage />}
       </main>
--- frontend/src/services/nutritionService.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-const API_URL = 'http://localhost:8080/api/nutrition/logs';
-
-export const getNutritionLogs = () => {
-  return axios.get(API_URL);
-};
