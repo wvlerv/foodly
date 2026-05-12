79acac1533bf50257ed8ae5ad0bb1ed6b3b01b2e
Valeria <valeriakhudaieva@outlook.com>
2026-05-12 20:25:11 +0300

    added health profile

--- backend/src/main/java/com/foodly/backend/entity/User.java
+++ backend/src/main/java/com/foodly/backend/entity/User.java
@@ -1,15 +1,7 @@
 package com.foodly.backend.entity;
 
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToOne;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+
 import java.util.UUID;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -42,7 +34,6 @@ public class User {
 	@Enumerated(EnumType.STRING)
 	private Role role;
 
-	@OneToOne(mappedBy = "user", cascade = CascadeType.ALL)
+	@OneToOne(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
 	private HealthProfile healthProfile;
-
 }
