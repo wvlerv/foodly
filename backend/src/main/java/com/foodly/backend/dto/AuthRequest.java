70a8cafdfbcf56445e322b7da27b45ee5e0bff8e
Valeria <valeriakhudaieva@outlook.com>
2026-05-12 18:37:00 +0300

    implement auth module with login, registration and JWT

---
+++ backend/src/main/java/com/foodly/backend/dto/AuthRequest.java
@@ -0,0 +1,9 @@
+package com.foodly.backend.dto;
+
+import lombok.Data;
+
+@Data
+public class AuthRequest {
+    private String email;
+    private String password;
+}
--- backend/src/main/java/com/foodly/backend/repository/UserRepository.java
+++ backend/src/main/java/com/foodly/backend/repository/UserRepository.java
@@ -9,6 +9,8 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
 
+	boolean existsByEmail(String email);
+
 	Optional<User> findByEmail(String email);
 
 }
